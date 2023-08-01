512add908bdd7ff4a14db28d7dc78e8677b84e12
bokim2 <bokim.ce@gmail.com>
2023-07-31 17:05:41 -0700

    need to correct time elapsed hr

--- client/components/FlaskList.jsx
+++ client/components/FlaskList.jsx
@@ -1,131 +1,154 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { FlasksContext } from '../context/FlasksContext'
 // import FlaskFinder from "../apis/FlaskFinder"
-import moment from 'moment';
-import { useNavigate } from 'react-router';
-moment().format();
-import { v4 as uuid } from 'uuid';
-import axios from 'axios';
-
-
+import moment from 'moment'
+import { useNavigate } from 'react-router'
+moment().format()
+import { v4 as uuid } from 'uuid'
+import axios from 'axios'
 
 function FlaskList() {
-    const {flasks, setFlasks} = useContext(FlasksContext);
-    const navigate = useNavigate();
-    // const [strain, setStrain] = useState("")
+  const { flasks, setFlasks } = useContext(FlasksContext)
+  const navigate = useNavigate()
+  // const [strain, setStrain] = useState("")
 
-    function navigateFlask(e, id){
-      // e.stopPropagation();
-      // setStrain(flask.strain)    
-      navigate(`/${id}`)
-    }
-  useEffect(()=>{
+  function navigateFlask(e, id) {
+    // e.stopPropagation();
+    // setStrain(flask.strain)
+    navigate(`/${id}`)
+  }
+  useEffect(() => {
     const fetchFlasks = async () => {
       let { data: response } = await axios.get(
-        process.env.NODE_ENV === 'production' 
-        ? `api/flasks`
-        : `http://localhost:4000/api/flasks/`)
+        process.env.NODE_ENV === 'production'
+          ? `api/flasks`
+          : `http://localhost:4000/api/flasks/`,
+      )
 
-    setFlasks(response.data.flasks)
-
+      setFlasks(response.data.flasks)
     }
-    fetchFlasks()
-    .catch(console.err)
-  },[])
-// console.log('flasks inside', flasks)
+    fetchFlasks().catch(console.err)
+  }, [])
+  // console.log('flasks inside', flasks)
   return (
     <div>
       <div className="list-group">
         <table className="table table-hover table-dark">
           <thead>
             <tr className="bg-primary table-warning">
-                <th className="text-center" scope="col">flask id</th>
-                <th className="text-center" scope="col">cell bank</th>
-                <th className="text-center" scope="col">inoculum uL</th>
-                <th className="text-center" scope="col">media mL</th>
-                <th className="text-center" scope="col">start YYYY-MM-DD</th>
-                <th className="text-center" scope="col">sample YYYY-MM-DD</th>
-                <th className="text-center" scope="col">completed</th>
-                <th className="text-center" scope="col">time elapsed hr</th>
-                <th className="text-center" scope="col">OD600</th>
-                <th className="text-center table-primary" scope="col">strain</th>
+              <th className="text-center" scope="col">
+                flask id
+              </th>
+              <th className="text-center" scope="col">
+                cell bank
+              </th>
+              <th className="text-center" scope="col">
+                inoculum uL
+              </th>
+              <th className="text-center" scope="col">
+                media mL
+              </th>
+              <th className="text-center" scope="col">
+                start YYYY-MM-DD
+              </th>
+              <th className="text-center" scope="col">
+                sample YYYY-MM-DD
+              </th>
+              <th className="text-center" scope="col">
+                completed
+              </th>
+              <th className="text-center" scope="col">
+                time elapsed hr
+              </th>
+              <th className="text-center" scope="col">
+                OD600
+              </th>
+              <th className="text-center table-primary" scope="col">
+                strain
+              </th>
             </tr>
           </thead>
 
           <tbody>
-            
-                {   flasks &&
-                    flasks.map(flask => {
-                        return (
-                            <tr key={uuid()} value={flask.id} onClick={(e)=> navigateFlask(e, flask.id)}>
-                                <td className="text-center">{flask.id}</td>
-                                <td className="text-center">{flask.cell_bank}</td>
-                                <td className="text-center">{flask.inoculum_ul}</td>
-                                <td className="text-center">{flask.media_ml}</td>
-                                <td className="text-center">{momentFormat(flask.start_date)}</td>
-                                <td className="text-center">{momentFormat(flask.end_date)}</td>
-                                <td className="text-center">{flask.completed}</td>
-                                <td className="text-center">{flask.time_since_inoc
-                                }</td>
-                                <td className="text-center">{flask.od600}</td>
-                                <td className="text-center table-light">{flask.strain}</td>
-                            </tr>
-                        )
-                    })
-                }
-           
+            {flasks &&
+              flasks.map(flask => {
+                return (
+                  <tr
+                    key={uuid()}
+                    value={flask.id}
+                    onClick={e => navigateFlask(e, flask.id)}
+                  >
+                    <td className="text-center">{flask.id}</td>
+                    <td className="text-center">{flask.cell_bank}</td>
+                    <td className="text-center">{flask.inoculum_ul}</td>
+                    <td className="text-center">{flask.media_ml}</td>
+                    <td className="text-center">
+                      {momentFormat(flask.start_date)}
+                    </td>
+                    <td className="text-center">
+                      {momentFormat(flask.end_date)}
+                    </td>
+                    <td className="text-center">{flask.completed}</td>
+                    <td className="text-center">{flask.time_since_inoc}</td>
+                    <td className="text-center">{flask.od600}</td>
+                    <td className="text-center table-light">{flask.strain}</td>
+                  </tr>
+                )
+              })}
           </tbody>
         </table>
       </div>
-
     </div>
   )
 }
 
-export function momentFormat(timestamp){
-  if(!timestamp){ return "" } else {return moment(timestamp).format('YYYY-MM-DD, h:mm:ss a')}
+export function momentFormat(timestamp) {
+  if (!timestamp) {
+    return ''
+  } else {
+    return moment(timestamp).format('YYYY-MM-DD, h:mm:ss a')
+  }
 }
 
-export function momentInterval(end_date, start_date){
-let a = moment(end_date);//now
-// console.log('end_date', end_date)
-// if (end_date){
-//   // let a = moment()
-//   let b = moment(new Date());
-// } else {
-//   let a = moment(end_date);
-// }
-let b = moment(start_date);
-// console.log('b', b)
-// console.log(a.diff(b, 'minutes')) // 44700
-// console.log("a.diff(b, 'hours')", a.diff(b, 'hours')) // 745
-let time_elapsed = a.diff(b, 'hours')
-if (isNaN(time_elapsed)){
-  time_elapsed = moment().diff(b, 'hours')
-}
-return time_elapsed
-// console.log(a.diff(b, 'days')) // 31
-// console.log(a.diff(b, 'weeks')) // 4
+export function momentInterval(end_date, start_date) {
+  let a = moment(end_date) //now
+  // console.log('end_date', end_date)
+  // if (end_date){
+  //   // let a = moment()
+  //   let b = moment(new Date());
+  // } else {
+  //   let a = moment(end_date);
+  // }
+  let b = moment(start_date)
+  // console.log('b', b)
+  // console.log(a.diff(b, 'minutes')) // 44700
+  // console.log("a.diff(b, 'hours')", a.diff(b, 'hours')) // 745
+  let time_elapsed = a.diff(b, 'hours')
+  if (isNaN(time_elapsed)) {
+    time_elapsed = moment().diff(b, 'hours')
+  }
+  return time_elapsed
+  // console.log(a.diff(b, 'days')) // 31
+  // console.log(a.diff(b, 'weeks')) // 4
 }
 
 export async function fetchFlasks() {
-let { data: response } = await axios.get(
-  process.env.NODE_ENV === 'production' 
-  ? `api/flasks`
-  : `http://localhost:4000/api/flasks/`)
-// let data = await response.json()
-// console.log(data)
-setFlasks(response.data.flasks)
-// console.log('response.data.flasks useEffect FlaskList', response.data.flasks)
+  let { data: response } = await axios.get(
+    process.env.NODE_ENV === 'production'
+      ? `api/flasks`
+      : `http://localhost:4000/api/flasks/`,
+  )
+  // let data = await response.json()
+  // console.log(data)
+  setFlasks(response.data.flasks)
+  // console.log('response.data.flasks useEffect FlaskList', response.data.flasks)
 }
 
-function timeSince (interval){
-  if(!interval) return ""
+function timeSince(interval) {
+  if (!interval) return ''
   // console.log(interval)
-  return  interval.days ||  interval.hours || 0
+  return interval.days || interval.hours || 0
   // || (interval.min && interval.min + " min")
 }
-
-
 
 export default FlaskList
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-//Our npm commands: 
+//Our npm commands:
 // npm init -y
 // npm i react react-dom
-// npm install -D babel-loader @babel/core @babel/preset-env @babel/preset-react  
+// npm install -D babel-loader @babel/core @babel/preset-env @babel/preset-react
 // npm i -D style-loader file-loader css-loader sass-loader sass
 // npm install webpack-dev-server --save-dev
 // npm i -D webpack webpack-cli
@@ -15,57 +15,60 @@
 // npm install cors
 // npm install moment --save
 
-
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const path = require('path');
-const db = require("./db/db.js");
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const db = require('./db/db.js')
 
 // const morgan = require('morgan');
-const app = express();
+const app = express()
 // const apiRouter = require('./routes.api');
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan('dev'));
-
-app.use(express.static(path.resolve(__dirname, '../build/')));
+app.use(express.static(path.resolve(__dirname, '../build/')))
 
 //GET all flasks and join table - new
-app.get("/api/flasksall/:id", async(req, res)=>{
-  try{
-    const results = await db.query("SELECT * FROM flasks f full outer JOIN samples s ON f.id = s.flask_id LEFT JOIN cell_banks c ON f.cell_bank = c.cell_bank WHERE id = $1 ORDER BY f.id DESC ", [req.params.id]);
+app.get('/api/flasksall/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'SELECT * FROM flasks f full outer JOIN samples s ON f.id = s.flask_id LEFT JOIN cell_banks c ON f.cell_bank = c.cell_bank WHERE id = $1 ORDER BY f.id DESC ',
+      [req.params.id],
+    )
     // LEFT OUTER JOIN cell_bank ON flasks.cell_bank=cell_bank.cell_bank ORDER BY flasks.id
     // console.log(results);
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: results.rows.length,
       data: {
-        flasks: results.rows
-      }
+        flasks: results.rows,
+      },
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
 
 //GET all flasks and join table - new
-app.get("/api/flasks", async(req, res)=>{
-  try{
-    const results = await db.query("SELECT * FROM flasks f full outer JOIN samples s ON f.id = s.flask_id LEFT JOIN cell_banks c ON f.cell_bank = c.cell_bank ORDER BY f.id DESC");
+app.get('/api/flasks', async (req, res) => {
+  try {
+    const results = await db.query(
+      'SELECT * FROM flasks f full outer JOIN samples s ON f.id = s.flask_id LEFT JOIN cell_banks c ON f.cell_bank = c.cell_bank ORDER BY f.id DESC',
+    )
     // LEFT OUTER JOIN cell_bank ON flasks.cell_bank=cell_bank.cell_bank ORDER BY flasks.id
     // console.log(results);
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: results.rows.length,
       data: {
-        flasks: results.rows
-      }
+        flasks: results.rows,
+      },
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
@@ -83,62 +86,76 @@ app.get("/api/flasks", async(req, res)=>{
 //         flasks: results.rows
 //       }
 //     })
-//   } catch(err){ 
+//   } catch(err){
 //     console.log(err)
 //   }
 // })
 
 //GET one flask
-app.get("/api/flasks/:id", async(req, res)=>{
-  try{
-    const results = await db.query("select * from flasks WHERE id = $1", [req.params.id]);
-    console.log('results of getting one flask', results.rows[0]);
+app.get('/api/flasks/:id', async (req, res) => {
+  try {
+    const results = await db.query('select * from flasks WHERE id = $1', [
+      req.params.id,
+    ])
+    console.log('results of getting one flask', results.rows[0])
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: results.rows.length,
       data: {
-        flasks: results.rows[0]
-      }
+        flasks: results.rows[0],
+      },
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
 
 //CREATE a flask
-app.post("/api/flasks", async(req, res)=>{
-  try{
+app.post('/api/flasks', async (req, res) => {
+  try {
     console.log('made it into create a flask server')
-    const results = await db.query("INSERT INTO flasks (cell_bank, inoculum_ul, media_ml) values ($1, $2, $3) returning *", [req.body.cell_bank, req.body.inoculum_ul, req.body.media_ml]);
-    console.log('create flask results', results);
+    const results = await db.query(
+      'INSERT INTO flasks (cell_bank, inoculum_ul, media_ml) values ($1, $2, $3) returning *',
+      [req.body.cell_bank, req.body.inoculum_ul, req.body.media_ml],
+    )
+    console.log('create flask results', results)
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: results.rows.length,
       data: {
-        flasks: results.rows[0]
-      }
+        flasks: results.rows[0],
+      },
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
 
 //take a timepoint
-app.post("/api/flasks/:id", async(req, res)=>{
-  try{
+app.post('/api/flasks/:id', async (req, res) => {
+  try {
     console.log('in update flask server')
     let date = new Date()
-    const results = await db.query("INSERT INTO samples (end_date, od600, completed, time_since_inoc, flask_id) values ($1, $2, $3, $4, $5) returning *", [req.body.end_date, req.body.od600, req.body.completed, req.body.time_since_inoc, req.body.flask_id]);
+    const results = await db.query(
+      'INSERT INTO samples (end_date, od600, completed, time_since_inoc, flask_id) values ($1, $2, $3, $4, $5) returning *',
+      [
+        req.body.end_date,
+        req.body.od600,
+        req.body.completed,
+        req.body.time_since_inoc,
+        req.body.flask_id,
+      ],
+    )
     // const results = await db.query("INSERT INTO samples cell_bank=$1, inoculum_ul=$2, media_ml=$3, end_date=$4, completed=$5, od600=$6 WHERE id=$7 returning *", [req.body.cell_bank, req.body.inoculum_ul, req.body.media_ml, date, req.body.completed, req.body.od600, req.params.id]);
-    console.log(results);
+    console.log(results)
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: results.rows.length,
       data: {
-        flasks: results.rows[0]
-      }
+        flasks: results.rows[0],
+      },
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
@@ -157,28 +174,28 @@ app.post("/api/flasks/:id", async(req, res)=>{
 //         flasks: results.rows[0]
 //       }
 //     })
-//   } catch(err){ 
+//   } catch(err){
 //     console.log(err)
 //   }
 // })
 
 //DELETE a flask
-app.delete("/api/flasks/:id", async(req, res)=>{
-  try{
-    const sample =  db.query("DELETE FROM samples WHERE flask_id = $1", [req.params.id]);
-    const results =  db.query("DELETE FROM flasks WHERE id = $1", [req.params.id]);
-    console.log(results);
+app.delete('/api/flasks/:id', async (req, res) => {
+  try {
+    const sample = db.query('DELETE FROM samples WHERE flask_id = $1', [
+      req.params.id,
+    ])
+    const results = db.query('DELETE FROM flasks WHERE id = $1', [
+      req.params.id,
+    ])
+    console.log(results)
     res.status(204).json({
-      status: "success"
+      status: 'success',
     })
-  } catch(err){ 
+  } catch (err) {
     console.log(err)
   }
 })
-
-
-
-
 
 //define route handlers
 // app.use('/api', apiRouter);
@@ -187,9 +204,9 @@ app.delete("/api/flasks/:id", async(req, res)=>{
 // app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on the route '/'
 app.get('/test', (req, res) => {
-  console.log("testing at /test");
-  return res.send("hi");
-});
+  console.log('testing at /test')
+  return res.send('hi')
+})
 
 // app.get('/', (req, res) => {
 //   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
@@ -198,13 +215,13 @@ app.get('/test', (req, res) => {
 /* global err handler */
 app.use((err, req, res, next) => {
   const defaultErr = {
-      log: 'express error handler caught global err handler',
-      status: 500,
-      message: {err: 'global error handler'}
+    log: 'express error handler caught global err handler',
+    status: 500,
+    message: { err: 'global error handler' },
   }
-  const errorObj = Object.assign({}, defaultErr, err);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+  const errorObj = Object.assign({}, defaultErr, err)
+  return res.status(errorObj.status).json(errorObj.message)
+})
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/'))
